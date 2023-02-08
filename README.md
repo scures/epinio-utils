e0c1e8dfc5955e82243d058358539ccf2582b5cc
scures <scurescu@suse.com>
2023-02-08 11:45:15 +0100

    Adds random sufix for items

---
+++ README.md
@@ -0,0 +1,10 @@
+# Epinio Utils
+
+Some scripts that helps out with Epinio development.
+
+#### Note
+You might want to give the files executable permissions before running them:
+
+```bash
+chmod u+x *.sh
+```
--- epinio-populate.sh
+++ epinio-populate.sh
@@ -3,22 +3,31 @@ echo "What would you like?  all | app | service | config"
 read varname
 echo Generating $varname resources
 
+OS=`uname`
+if [ "$OS" = 'Darwin' ]; then
+    export LC_ALL=C
+fi
+
 function buildApplications {
-  epinio push -n sample --container-image-url splatform/sample-app
-  epinio push -n sample-2 --container-image-url splatform/sample-app
+  epinio push -n sample-$(generate_random_string) --container-image-url splatform/sample-app
+  epinio push -n sample-$(generate_random_string) --container-image-url splatform/sample-app
   exit 0
 }
 
 function buildServices {
-  epinio service create mongodb-dev test-mongo-one
-  epinio service create mongodb-dev test-mongo-two
+  epinio service create mongodb-dev test-mongo-$(generate_random_string)
+  epinio service create mongodb-dev test-mongo-$(generate_random_string)
   exit 0
 }
 
 function buildConfigurations() {
-  epinio configuration create config-one foo bar
-  epinio configuration create config-two foo bar
+  epinio configuration create config-$(generate_random_string) foo bar
+  epinio configuration create config-$(generate_random_string) foo bar
   exit 0
+}
+
+generate_random_string() {
+  echo $(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)
 }
 
 
