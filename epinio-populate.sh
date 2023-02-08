08cbf4d60f59e000b310f43223b0a6a7e39b4177
scures <scurescu@suse.com>
2023-02-08 11:55:04 +0100

    Small improvements and fixes

--- epinio-populate.sh
+++ epinio-populate.sh
@@ -11,19 +11,16 @@ fi
 function buildApplications {
   epinio push -n sample-$(generate_random_string) --container-image-url splatform/sample-app
   epinio push -n sample-$(generate_random_string) --container-image-url splatform/sample-app
-  exit 0
 }
 
 function buildServices {
   epinio service create mongodb-dev sample-mongo-$(generate_random_string)
   epinio service create mongodb-dev sample-mongo-$(generate_random_string)
-  exit 0
 }
 
 function buildConfigurations() {
   epinio configuration create sample-config-$(generate_random_string) foo bar
   epinio configuration create sample-config-$(generate_random_string) foo bar
-  exit 0
 }
 
 generate_random_string() {
@@ -32,19 +29,24 @@ generate_random_string() {
 
 
 if [ "$varname" = "all" ]; then
-  buildApplications
   buildServices
   buildConfigurations
-fi
-
-if [ "$varname" = "app" ] || [ "$varname" = "all" ]; then
   buildApplications
+
+  exit 0
 fi
 
-if [ "$varname" = "service" ] || [ "$varname" = "all" ]; then
+if [ "$varname" = "app" ]; then
+  buildApplications
+  exit 0
+fi
+
+if [ "$varname" = "service" ]; then
   buildServices
+  exit 0
 fi
 
-if [ "$varname" = "config" ] || [ "$varname" = "all" ]; then
+if [ "$varname" = "config" ]; then
   buildConfigurations
+  exit 0
 fi
