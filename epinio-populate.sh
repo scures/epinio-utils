422c23e8a76ba2ecd492ddf4891be1ba450a8a89
scures <scurescu@suse.com>
2023-02-10 14:50:28 +0100

    Allows for namespaces

--- epinio-populate.sh
+++ epinio-populate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "What would you like?  all | app | service | config"
-read varname
-echo Generating $varname resources
+echo -n "💠 What would you like?  all | app | service | config | ns => "
+read type
+echo Generating $type resources
 
 OS=`uname`
 if [ "$OS" = 'Darwin' ]; then
@@ -9,8 +9,8 @@ if [ "$OS" = 'Darwin' ]; then
 fi
 
 function buildApplications {
-  epinio push -n sample-$(generate_random_string) --container-image-url splatform/sample-app
-  epinio push -n sample-$(generate_random_string) --container-image-url splatform/sample-app
+  epinio push -n sample-app-$(generate_random_string) --container-image-url splatform/sample-app
+  epinio push -n sample-app-$(generate_random_string) --container-image-url splatform/sample-app
 }
 
 function buildServices {
@@ -23,30 +23,41 @@ function buildConfigurations() {
   epinio configuration create sample-config-$(generate_random_string) foo bar
 }
 
+function buildNamespaces() {
+  epinio namespace create sample-ns-$(generate_random_string)
+  epinio namespace create sample-ns-$(generate_random_string)
+}
+
 generate_random_string() {
   echo $(cat /dev/urandom | tr -dc 'a-z' | fold -w 8 | head -n 1)
 }
 
 
-if [ "$varname" = "all" ]; then
+if [ "$type" = "all" ]; then
   buildServices
   buildConfigurations
   buildApplications
+  buildNamespaces
 
   exit 0
 fi
 
-if [ "$varname" = "app" ]; then
+if [ "$type" = "app" ]; then
   buildApplications
   exit 0
 fi
 
-if [ "$varname" = "service" ]; then
+if [ "$type" = "service" ]; then
   buildServices
   exit 0
 fi
 
-if [ "$varname" = "config" ]; then
+if [ "$type" = "config" ]; then
   buildConfigurations
+  exit 0
+fi
+
+if [ "$type" = "ns" ]; then
+  buildNamespaces
   exit 0
 fi
